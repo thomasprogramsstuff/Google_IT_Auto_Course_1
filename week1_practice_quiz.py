1326fa60eb1bba034cd23a66737ad62950b8324c
TCHI <87097016+thomasprogramsstuff@users.noreply.github.com>
2022-02-15 10:03:28 -0500

    Create week1_practice_quiz.py
    Getting started with Python code.
    Python uses ** for exponents/powers, NOT ^

---
+++ week1_practice_quiz.py
@@ -0,0 +1,8 @@
+# 4. Output a message that says "Programming in Python is fun!" to the screen.
+print("Programming in Python is fun!")
+
+# 5. Replace the ___ placeholder and calculate the Golden ratio:  1+52\frac{1+\sqrt{5}}{2}21+5
+(1+(5)^(1/2))/2
+# Tip: to calculate the square root of a number xxx, you can use x**(1/2).
+ratio = (1+((5)**(1/2)))/2
+print(ratio)

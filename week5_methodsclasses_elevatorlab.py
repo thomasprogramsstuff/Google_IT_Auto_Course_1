67f0f71108c04b1515ba8137024a9e2bf68c2871
TCHI <87097016+thomasprogramsstuff@users.noreply.github.com>
2022-02-17 13:13:19 -0500

    Create week5_methodsclasses_elevatorlab.py

---
+++ week5_methodsclasses_elevatorlab.py
@@ -0,0 +1,50 @@
+#
+class Elevator:
+    def __init__(self, bottom, top, current):
+        """Initializes the Elevator instance."""
+        self.current = 0
+        self.bottom = 0
+        self.top = 10
+    def up(self):
+        """Makes the elevator go up one floor."""
+        if self.current < 10:
+            self.current+=1
+        else:
+            self.current = 10
+    def down(self):
+        """Makes the elevator go down one floor."""
+        if self.current > 0:
+            self.current-=1
+        else:
+            self.current = 0
+    def go_to(self, floor):
+        """Makes the elevator go to the specific floor."""
+        self.current = floor
+
+elevator = Elevator(-1, 10, 0)
+
+#test cases:
+elevator.up() 
+elevator.current #should output 1
+
+elevator.down() 
+elevator.current #should output 0
+
+elevator.go_to(10) 
+elevator.current #should output 10
+
+# Go to the top floor. Try to go up, it should stay. Then go down.
+elevator.go_to(10)
+elevator.up()
+elevator.down()
+print(elevator.current) # should be 9
+# Go to the bottom floor. Try to go down, it should stay. Then go up.
+elevator.go_to(-1)
+elevator.down()
+elevator.down()
+elevator.up()
+elevator.up()
+print(elevator.current) # should be 1
+
+elevator.go_to(5)
+print(elevator)
